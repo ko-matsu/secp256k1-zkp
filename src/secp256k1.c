2f8f802961af1857eedf38a1dcdcb84c18581bc7
k-matsuzawa <matsuzawa@cryptogarage.co.jp>
2020-12-22 12:08:57 +0900

    feat: merge ECDSA adaptor & secp256k1_schnorrsig_compute_sigpoint

--- src/secp256k1.c
+++ src/secp256k1.c
@@ -805,3 +805,6 @@ int secp256k1_ec_pubkey_combine(const secp256k1_context* ctx, secp256k1_pubkey *
 # include "modules/surjection/main_impl.h"
 #endif
 
+#ifdef ENABLE_MODULE_ECDSA_ADAPTOR
+# include "modules/ecdsa_adaptor/main_impl.h"
+#endif
--- src/tests.c
+++ src/tests.c
@@ -5620,6 +5620,10 @@ void run_ecdsa_openssl(void) {
 # include "modules/schnorrsig/tests_impl.h"
 #endif
 
+#ifdef ENABLE_MODULE_ECDSA_ADAPTOR
+# include "modules/ecdsa_adaptor/tests_impl.h"
+#endif
+
 void run_secp256k1_memczero_test(void) {
     unsigned char buf1[6] = {1, 2, 3, 4, 5, 6};
     unsigned char buf2[sizeof(buf1)];
@@ -5927,6 +5931,11 @@ int main(int argc, char **argv) {
 
 #ifdef ENABLE_MODULE_SCHNORRSIG
     run_schnorrsig_tests();
+#endif
+
+#ifdef ENABLE_MODULE_ECDSA_ADAPTOR
+    /* ECDSA pubkey recovery tests */
+    run_ecdsa_adaptor_tests();
 #endif
 
     /* util tests */
